b8586c7cb14d54ba9268027b1582fbe1064c7d4d
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-07 07:02:34 +0300

    Added shadow to login button Added corner radius for login button

--- LoginScreen/LoginScreen/ViewController.swift
+++ LoginScreen/LoginScreen/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureButton()
     }
 
-
+    // MARK: - Methods
+    private func configureButton() {
+        // Add shadow
+        loginButton.layer.shadowColor = UIColor.systemIndigo.cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        loginButton.layer.shadowOpacity = 1
+    }
 }
 
